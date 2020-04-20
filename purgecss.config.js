899d8489d38ec7dd0bf1e1df01564bbbad306545
Mojtaba Javan <javan.mojtaba@gmail.com>
2020-04-20 11:28:48 +0430

    Trigger Travis

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   content: ["src/**/*.njk", "src/**/*.js", "src/*.md"],
   css: ["src/assets/css/site.css"],
-  whitelistPatterns: [/hljs|hljs\-.*/]
+  whitelistPatterns: [/hljs|hljs\-.*/],
 };
