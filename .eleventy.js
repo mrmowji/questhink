8fed9acfb595d9e4e10904c146a5687f7b092673
Mojtaba Javan <mojtaba@avicennaresearch.com>
2026-08-21 14:41:25 +0330

    Remove GA and SC stuff

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/manifest.json");
   eleventyConfig.addPassthroughCopy("src/CNAME");
-  eleventyConfig.addPassthroughCopy("src/google2c4932720a20e47e.html");
 
   eleventyConfig.setTemplateFormats("md,njk");
 
